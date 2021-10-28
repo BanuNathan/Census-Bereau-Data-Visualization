d4c7d264daedc3af8e7e8b1244af50f73c62952f
Banu Nathan <banuswami1996@gmail.com>
2021-10-28 06:50:54 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 function makeResponsive() {
 
     // if the SVG area isn't empty when the browser loads,
-    // remove it and replace it with a resized version of the chart
+    // remove it and replace it with a resized version of the Chart
     var svgArea = d3.select("body").select("svg");
     var povertyData = [];
     d3.csv("assets/data/data.csv").then(function(povertyList, err) {
